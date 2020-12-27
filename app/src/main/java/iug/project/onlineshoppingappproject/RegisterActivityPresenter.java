c277b4ed682262344e61f40f3766bf8a706c19ba
YousefJarada <43300465+YousefAiman@users.noreply.github.com>
2020-12-27 22:46:16 +0200

    Added Register Testing Implementation

--- app/src/main/java/iug/project/onlineshoppingappproject/RegisterActivityPresenter.java
+++ app/src/main/java/iug/project/onlineshoppingappproject/RegisterActivityPresenter.java
@@ -15,6 +15,26 @@ public class RegisterActivityPresenter implements
 
     @Override
     public void checkUserRegistrationInput(String email, String username, String password, String confirmPassword) {
+        if(email.trim().isEmpty()){
+        view.printErrorMessage("Email field is empty");
+        }else if(username.trim().isEmpty()){
+        view.printErrorMessage("Username field is empty");
+        }else if(password.trim().isEmpty()){
+        view.printErrorMessage("Password field is empty");
+        }else if(confirmPassword.trim().isEmpty()){
+        view.printErrorMessage("Confirm Password field is empty");
+        }else{
+
+            if(password.length() < 6){
+                view.printErrorMessage("Password is less than six characters");
+            }else {
+                if (!password.equals(confirmPassword)) {
+                    view.printErrorMessage("Password doesn't match");
+                } else {
+                    view.printSuccessMessage("Registration Success");
+                }
+            }
+        }
 
     }
 }
