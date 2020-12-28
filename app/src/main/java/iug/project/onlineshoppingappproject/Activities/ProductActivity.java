1ac9e40db13adac31d96f05a49e862c63b4fe22e
YousefJarada <43300465+YousefAiman@users.noreply.github.com>
2020-12-28 20:04:51 +0200

    Product item design and recyclers

--- app/src/main/java/iug/project/onlineshoppingappproject/Activities/ProductActivity.java
+++ app/src/main/java/iug/project/onlineshoppingappproject/Activities/ProductActivity.java
@@ -68,7 +68,9 @@ public class ProductActivity extends AppCompatActivity {
 
 
             productsViewPager.addOnPageChangeListener(new ViewPager.OnPageChangeListener() {
+
                 @Override
+
                 public void onPageScrolled(int position, float positionOffset, int positionOffsetPixels) {
 
                 }
